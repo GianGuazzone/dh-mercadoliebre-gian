55a132527812b0bf2ae12029025db1a347d87572
Gian Guazzone <gianguazzone@MacBook-Pro-de-Gian.local>
2022-05-19 20:48:03 -0300

    firs

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+     console.log("servidor corriendo" + PORT);
+    });
+
+    app.use(express.static(path.join(__dirname, "public")));
+
+    // camino, path, ruta
+    app.get('/', (req, res) => {
+        //función controladora o handler.
+        res.sendFile(path.join(__dirname,"views/home.html"));
+    });
+
+    app.get("/login", (req, res) => {
+        res.sendFile(path.join(__dirname,"views/login.html"));
+    });
+
+    app.get("/register", (req, res) => {
+        res.sendFile(path.join(__dirname,"views/register.html"));
+    });
