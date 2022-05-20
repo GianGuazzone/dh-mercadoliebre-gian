7a304ba282f3fdcf482a3effb471e577fcc8449f
Gian <gianguazzone@hotmail.com>
2022-05-19 21:19:31 -0300

    fixed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const path = require ('path');
 
-const PORT = proces.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
      console.log("servidor corriendo" + PORT);
